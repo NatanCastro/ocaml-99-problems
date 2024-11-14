c3a7b5b3723c3dd17b2034c931e2ce6463254b70
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2024-11-14 02:07:02 -0300

    init project

---
+++ bin/main.ml
@@ -0,0 +1,9 @@
+let () = 
+  print_endline "seventeen problem";
+  Seventeen.run ();
+  print_endline "eighteen problem";
+  Eighteen.run ();
+  print_endline "nineteen problem";
+  Nineteen.run ();
+  print_endline "twenty problem";
+  Twenty.run ();
